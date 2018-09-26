3ce1303450a36c7a92a2c95d226c51012f2585d4
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-09-26 16:56:42 -0700

    Minion and MinionTest(WIP)

---
+++ src/Minion.java
@@ -0,0 +1,51 @@
+
+public class Minion {
+String name = ""; 
+int eyes = 0; 
+String color = ""; 
+String master = "";
+	
+	
+	
+	public void getName() {
+		System.out.println(name);
+		
+	}
+	
+	public void getEyes() {
+		System.out.println(eyes);
+	}
+	
+	public void getColor() {
+		System.out.println(color);
+	}
+	
+	public void getMaster() {
+		System.out.println(master);
+	}
+	
+	public void setMaster(String master) {
+		
+	}
+	
+	public void assertEquals() {
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	public Minion (String name, int eyes, String color, String master) {
+	this.name = name;
+	this.eyes = eyes;
+	this.color = color;
+	this.master = master;
+	}
+	
+	
+	
+}
