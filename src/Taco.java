66abb7a1a0ea1876cde778736db9599194a50239
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-10-17 16:50:59 -0700

    Taco

---
+++ src/Taco.java
@@ -0,0 +1,41 @@
+
+public class Taco {
+private String meat;
+private String sauce;
+	
+	
+	String getMeat(){
+		return meat;
+	}
+	
+	String getSauce(){
+		return sauce;
+	}
+	
+	 void setMeat(String meat) {
+		this.meat = meat;
+	}
+	
+	void setSauce(String sauce) {
+		this.sauce = sauce;
+	}
+	
+	
+	
+	
+	
+	
+	public Taco(String meat, String sauce) {
+		this.meat = meat;
+		this.sauce = sauce;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
